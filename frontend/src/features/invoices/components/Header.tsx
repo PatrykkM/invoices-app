2b8a549e3dbce47ce39f4b07299236c4394a6202
PatrykkM <patrykmrzyglod3@gmail.com>
2025-09-25 20:40:43 +0200

    init spring boot backend

--- frontend/src/features/invoices/components/Header.tsx
+++ frontend/src/features/invoices/components/Header.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { Button } from "@/src/components/ui/button";
 import IacText from "@/src/components/ui/IacText";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <div className="flex justify-between">
       <IacText text="All Invoices" size="2xl" weight="bold" />
@@ -11,6 +16,7 @@ const Header = () => {
         variant={"accent"}
         size={"lg"}
         className="self-start font-bold"
+        onClick={() => router.push("/home")}
       >
         Create new invoice
       </Button>
--- frontend/src/features/invoices/components/InvoicesTable.tsx
+++ frontend/src/features/invoices/components/InvoicesTable.tsx
@@ -9,38 +9,70 @@ import {
   TableRow,
 } from "@/src/components/ui/table";
 
+interface TableData {
+  invoiceNumber: string;
+  issueDate: string;
+  dueDate: string;
+  NIP: string;
+  totalGrossPrice: number;
+}
+
 export function InvoicesTable({
   items,
 }: {
   items?: { description?: string; quantity?: number; netPrice?: number }[];
 }) {
-  const mockData = [
-    { description: "Item 1", quantity: 2, netPrice: 50 },
-    { description: "Item 2", quantity: 1, netPrice: 100 },
-    { description: "Item 3", quantity: 5, netPrice: 20 },
+  const mockData: TableData[] = [
+    {
+      invoiceNumber: "Item 1",
+      issueDate: "2023-10-10",
+      dueDate: "2023-10-20",
+      NIP: "123-456-78-90",
+      totalGrossPrice: 100,
+    },
+    {
+      invoiceNumber: "Item 2",
+      issueDate: "2023-11-01",
+      dueDate: "2023-11-10",
+      NIP: "123-456-78-90",
+      totalGrossPrice: 100,
+    },
+    {
+      invoiceNumber: "Item 3",
+      issueDate: "2023-12-01",
+      dueDate: "2023-12-20",
+      NIP: "123-456-78-90",
+      totalGrossPrice: 100,
+    },
   ];
 
   return (
     <Table>
       <TableHeader>
         <TableRow>
-          <TableHead>Description</TableHead>
-          <TableHead>Qty</TableHead>
-          <TableHead>Price</TableHead>
+          <TableHead>Invoice Number</TableHead>
+          <TableHead>Issue Date</TableHead>
+          <TableHead>Due Date</TableHead>
+          <TableHead>NIP</TableHead>
+          <TableHead>Total Gross Price</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {mockData?.map((item, i) => (
           <TableRow key={i} className="h-14">
             <TableCell className="whitespace-normal font-medium [overflow-wrap:anywhere]">
-              {item.description}
+              {item.invoiceNumber}
             </TableCell>
-            <TableCell className="w-20 [overflow-wrap:anywhere]">
-              {item.quantity}
+            <TableCell className="whitespace-normal font-medium [overflow-wrap:anywhere]">
+              {item.NIP}
             </TableCell>
-            <TableCell className="w-28 [overflow-wrap:anywhere]">
-              ${item.netPrice}
+            <TableCell className="whitespace-normal font-medium [overflow-wrap:anywhere]">
+              {item.issueDate}
             </TableCell>
+            <TableCell className="whitespace-normal font-medium [overflow-wrap:anywhere]">
+              {item.dueDate}
+            </TableCell>
+            <TableCell>{item.totalGrossPrice}</TableCell>
           </TableRow>
         ))}
       </TableBody>
